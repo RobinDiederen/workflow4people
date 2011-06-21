a4daac35598693a0af176aa81a6b7d81704f96b1
Jacques Gehlen <jacques@gehlen-nb.(none)>
2011-06-21 09:16:41 +0200

    Database configuration backport / commons-httpclient-3.0.1.jar added

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,26 +1,38 @@
-	hibernate {
+dataSource {
+    pooled = true
+    driverClassName = "org.hsqldb.jdbcDriver"
+    username = "sa"
+    password = ""
+}
+hibernate {
     cache.use_second_level_cache=true
-    cache.use_query_cache=true    
+    cache.use_query_cache=true
     cache.provider_class='net.sf.ehcache.hibernate.EhCacheProvider'
     max_fetch_depth=5
 }
-
 // environment specific settings
 environments {
-	development {
+    development {
+
         dataSource {
             dbCreate = "update" // one of 'create', 'create-drop','update'
-            url="jdbc:mysql://localhost/wf4p_24052011"
-    		username = "dev"
-    		password = "dev"
-        } 
-	production {
+            jndiName = "java:comp/env/jdbc/wf4p"
+
+            dialect = "org.hibernate.dialect.MySQLInnoDBDialect"
+
+        }
+    }
+    test {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:hsqldb:mem:testDb"
+        }
+    }
+    production {
         dataSource {
             dbCreate = "update" // one of 'create', 'create-drop','update'
-            url="jdbc:mysql://localhost/wf4p_24052011"
-    		username = "dev"
-    		password = "dev"
-        } 
-	}
-}
+            jndiName = "java:comp/env/jdbc/wf4p"
+            dialect = "org.hibernate.dialect.MySQLInnoDBDialect"
+        }
+    }
 }
